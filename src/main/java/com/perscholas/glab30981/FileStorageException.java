95ec132f76d823445398284370183f32620302b5
Marlie Martinez <marlieahue@gmail.com>
2024-08-27 10:55:20 -0400

    first commit

---
+++ src/main/java/com/perscholas/glab30981/FileStorageException.java
@@ -0,0 +1,19 @@
+package com.perscholas.glab30981;
+
+public class FileStorageException extends RuntimeException {
+
+    private String msg;
+
+    public FileStorageException(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
+
